c6b6aff8374d081e5a496f2a6565fde05f3212a5
ChoiByeongMin <coqoa28@gmail.com>
2022-09-26 23:39:34 +0900

    naver maps 연동

--- android/app/src/main/java/com/sideprject/map/MainActivity.java
+++ android/app/src/main/java/com/sideprject/map/MainActivity.java
@@ -3,4 +3,10 @@ package com.sideprject.map;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+    // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+    //     if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+    //         requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0);
+    //     }
+    // }
+    // 이거 넣어야하나?
 }
